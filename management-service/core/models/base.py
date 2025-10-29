22399384f223dd119df9449cb8c342a1bd0e7c1e
Artur Tokranov <a.tokranov@bars.group>
2025-10-29 21:02:04 +0300

    Убрал поля из базовой модели

--- management-service/core/models/base.py
+++ management-service/core/models/base.py
@@ -1,20 +1,7 @@
-from datetime import (
-    UTC,
-    datetime,
-)
-
-from sqlalchemy import func
 from sqlalchemy.orm import (
     DeclarativeBase,
-    Mapped,
     declared_attr,
-    mapped_column,
 )
-
-
-def get_current_dt() -> datetime:
-    dt = datetime.now(tz=UTC)
-    return dt.replace(microsecond=0, tzinfo=None)
 
 
 class Base(DeclarativeBase):
@@ -23,9 +10,3 @@ class Base(DeclarativeBase):
     @declared_attr.directive
     def __tablename__(cls) -> str:  # noqa: N805
         return f"{cls.__name__.lower()}s"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    created_at: Mapped[datetime] = mapped_column(
-        default=get_current_dt,
-        server_default=func.now(),
-    )
