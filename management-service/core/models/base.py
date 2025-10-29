ffae6f86bb8befb208c42dc204e54f16bdc0534d
Artur Tokranov <a.tokranov@bars.group>
2025-10-29 21:35:26 +0300

    Добавил naming_convention в базовую модель

--- management-service/core/models/base.py
+++ management-service/core/models/base.py
@@ -1,11 +1,20 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import (
     DeclarativeBase,
     declared_attr,
 )
 
+from core import settings
+
 
 class Base(DeclarativeBase):
+    """Базовая модель."""
+
     __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.postgres.naming_convention,
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:  # noqa: N805
