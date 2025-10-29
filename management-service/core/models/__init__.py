abfd8b72054d49de5a3c316f5c8cf0d85aaf408e
Artur Tokranov <a.tokranov@bars.group>
2025-10-29 22:31:19 +0300

    Добавил модель помощник для работы с БД

--- management-service/core/models/__init__.py
+++ management-service/core/models/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
+    "db_helper",
     "Base",
     "Task",
 )
 
+from .db_helper import db_helper
 from .base import Base
 from .task import Task
---
+++ management-service/core/models/db_helper.py
@@ -0,0 +1,51 @@
+__all__ = ("db_helper",)
+
+import logging
+
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from core import settings
+
+log = logging.getLogger(__name__)
+
+
+class DatabaseHelper:
+    def __init__(
+        self,
+        url: str,
+        echo: bool = False,  # noqa: FBT001, FBT002
+        echo_pool: bool = False,  # noqa: FBT001, FBT002
+        pool_size: int = 5,
+        max_overflow: int = 10,
+    ) -> None:
+        self.engine: AsyncEngine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
+        )
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    async def dispose(self) -> None:
+        await self.engine.dispose()
+        log.info("Database engine disposed")
+
+
+db_helper = DatabaseHelper(
+    url=settings.postgres.url,
+    echo=settings.postgres.echo,
+    echo_pool=settings.postgres.echo_pool,
+    pool_size=settings.postgres.pool_size,
+    max_overflow=settings.postgres.max_overflow,
+)
