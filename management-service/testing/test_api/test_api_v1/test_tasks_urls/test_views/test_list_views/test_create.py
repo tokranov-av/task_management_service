ee889374dc489377685e0174640ffcd96e1cf93e
Artur Tokranov <a.tokranov@bars.group>
2025-11-10 17:11:08 +0300

    Подкорректировал тест создания задачи

--- management-service/testing/test_api/test_api_v1/test_tasks_urls/test_views/test_list_views/test_create.py
+++ management-service/testing/test_api/test_api_v1/test_tasks_urls/test_views/test_list_views/test_create.py
@@ -1,6 +1,9 @@
+from unittest.mock import AsyncMock
+
 import pytest
 from fastapi import status
 from httpx import AsyncClient
+from pytest_mock import MockerFixture
 
 from core.enums import TaskPriority
 from core.schemas import TaskCreate
@@ -9,7 +12,11 @@ from testing.utils import get_random_string
 
 
 @pytest.mark.apitest
-async def test_create_task(client: AsyncClient) -> None:
+async def test_create_task(client: AsyncClient, mocker: MockerFixture) -> None:
+    mock_publish = mocker.patch(
+        "api.api_v1.tasks_urls.views.list_views.rabbitmq_service.publish_task",
+        new_callable=AsyncMock,
+    )
     url = app.url_path_for("create_task")
     task_create = TaskCreate(
         title=get_random_string(),
@@ -20,6 +27,7 @@ async def test_create_task(client: AsyncClient) -> None:
     response = await client.post(url=url, json=task_create)
 
     assert response.status_code == status.HTTP_201_CREATED, response.text
+    mock_publish.assert_called_once()
     response_data = response.json()
     assert "id" in response_data
     assert response_data["title"] == task_create["title"]
